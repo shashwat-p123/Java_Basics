41df54fb343052b9f114494b1d0cbd37fc481964
Shashwat Bajpai <Shashwatcbajpai@gmail.com>
2025-08-07 23:34:10 +0530

    5_methods

---
+++ SimpleInterest.java
@@ -0,0 +1,83 @@
+// public class StudentInfo {
+    
+//     public void displayInfo() {
+//         System.out.println("Name: Aman Sharma");
+//         System.out.println("Age: 16");
+//         System.out.println("Grade: 10th");
+//     }
+
+//     public static void main(String[] args) {
+//         StudentInfo student = new StudentInfo();
+
+//         student.displayInfo();
+//     }
+// }
+
+
+
+
+// public class Calculator {
+
+//     public void addNumbers() {
+//         int number1 = 15;  
+//         int number2 = 25;  
+
+//         int sum = number1 + number2;
+
+//         System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
+//     }
+
+//     public static void main(String[] args) {
+//         Calculator calc = new Calculator();
+//         calc.addNumbers();
+//     }
+// }
+
+
+
+// public class Greeting {
+
+//     public void sayHello(String name) {
+//         System.out.println("Hello, " + name + "! Welcome to Java programming.");
+//     }
+
+//     public static void main(String[] args) {
+//         Greeting g = new Greeting();
+//         g.sayHello("Shashwat");
+//     }
+// }
+
+
+// public class Circle {
+
+//     public void calculateArea() {
+//         double radius = 7;
+//         double pi = 3.14;
+//         double area = pi * radius * radius;
+//         System.out.println("Area of the circle is: " + area);
+//     }
+
+//     public static void main(String[] args) {
+//         Circle c = new Circle();
+//         c.calculateArea();
+//     }
+// }
+
+
+
+// public class SimpleInterest {
+
+//     public void calculateInterest() {
+//         double principal = 1000;
+//         double time = 2;
+//         double rate = 5;
+//         double interest = (principal * time * rate) / 100;
+//         System.out.println("Simple Interest is: " + interest);
+//     }
+
+//     public static void main(String[] args) {
+//         SimpleInterest si = new SimpleInterest();
+//         si.calculateInterest();
+//     }
+// }
+
