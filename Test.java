b4efc9c0a21da7a893d77e684a0aa79cfced500d
Shashwat Bajpai <Shashwatcbajpai@gmail.com>
2025-08-08 00:03:19 +0530

    5_methods

---
+++ Test.java
@@ -0,0 +1,34 @@
+//printing hello world//
+
+// public class Test {
+// public static void main(String[] args){
+//     System.out.println("hello,javafrom vs code !");
+// }
+// }
+   
+
+// public class Test {
+//     public static void main(String[] args) {
+//         System.out.println("hii my name is britansh");
+//         System.out.print("pandey");
+//     }
+// }
+
+//==========================================================================================//
+
+//println and print command
+
+//  public class Test{
+//  public static void main(String[] args){
+//         System.out.println("britansh");
+//         System.out.print("18+");
+//     }
+// }
+
+   
+
+
+
+ 
+
+
